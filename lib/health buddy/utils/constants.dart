a6d4cf40d15ee730e1d3bc84d37e20dd4a024831
crazycoder534 <crazycoder534@gmail.com>
2022-08-02 16:34:06 +0500

    created some custom widgets, routs, colors and textstyles

---
+++ lib/health buddy/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//COLORS
+Color blue500 = Colors.lightBlue[500]!;
+Color blue100 = Colors.lightBlue[100]!;
+Color grey100 = Colors.grey[100]!;
+Color whiteColor = Colors.white;
+
+//TEXT STYLES
+TextStyle titleTextStyle = const TextStyle(
+    color: Colors.black, fontSize: 20, fontWeight: FontWeight.w700);
+TextStyle subTitleTextStyle = TextStyle(
+    color: Colors.grey[400], fontSize: 16, fontWeight: FontWeight.w400);
+TextStyle btnTextStyle = const TextStyle(
+    color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500);
+TextStyle smallTextStyle = TextStyle(
+    color: Colors.grey[500], fontSize: 12, fontWeight: FontWeight.w400);
+TextStyle prefixStyle = const TextStyle(
+    color: Colors.black, fontSize: 16, fontWeight: FontWeight.w400);
---
+++ lib/health buddy/utils/widgets.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:health_buddy/health%20buddy/constants.dart';
+import 'package:svg_icon/svg_icon.dart';
+
+class CHIButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String btnLabel;
+  final bool expanded;
+  const CHIButton(
+      {Key? key,
+      required this.onTap,
+      required this.btnLabel,
+      this.expanded = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: !expanded ? MediaQuery.of(context).size.width * 0.5 : null,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+        height: 48,
+        decoration: BoxDecoration(
+          color: blue500,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(btnLabel, style: btnTextStyle),
+      ),
+    );
+  }
+}
+
+class ArrowButton extends StatelessWidget {
+  final bool leftAngle;
+  final VoidCallback onTap;
+  const ArrowButton({Key? key, this.leftAngle = true, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: blue500,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: SvgIcon(
+          leftAngle ? 'assets/icons/back.svg' : 'assets/icons/next.svg',
+          color: whiteColor,
+        ),
+      ),
+    );
+  }
+}
+
+class PhoneField extends StatelessWidget {
+  final bool isForPhone;
+  final List<String> items;
+  final void Function(String?)? onChanged;
+  final String value;
+  final String hintText;
+  const PhoneField(
+      {Key? key,
+      required this.items,
+      this.onChanged,
+      required this.value,
+      this.isForPhone = true,
+      required this.hintText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      height: 56,
+      decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          border: Border.all(color: grey100, width: 1),
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 0),
+              color: Colors.black.withOpacity(0.06),
+              blurRadius: 31,
+              spreadRadius: -6,
+            ),
+            BoxShadow(
+              offset: const Offset(0, 0),
+              color: Colors.black.withOpacity(0.06),
+              blurRadius: 31,
+              spreadRadius: -6,
+            ),
+          ]),
+      child: Row(
+        children: [
+          isForPhone
+              ? DropdownButton(
+                  underline: const SizedBox.shrink(),
+                  icon: const SvgIcon('assets/icons/arrow_down.svg'),
+                  iconSize: 8,
+                  value: value,
+                  items: items.map((String item) {
+                    return DropdownMenuItem(
+                      value: item,
+                      child: Text(item),
+                    );
+                  }).toList(),
+                  onChanged: onChanged,
+                )
+              : const SizedBox.shrink(),
+          Expanded(
+            child: TextField(
+              keyboardType:
+                  isForPhone ? TextInputType.phone : TextInputType.name,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.only(left: 8),
+                hintText: hintText,
+                hintStyle: subTitleTextStyle,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CHIRouter {
+  static push(BuildContext context, Widget nextPage) {
+    return Navigator.push(
+        context, MaterialPageRoute(builder: (context) => nextPage));
+  }
+
+  static pushReplacement(BuildContext context, Widget nextPage) {
+    return Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => nextPage));
+  }
+
+  static pushNamed(BuildContext context, String routeName) {
+    return Navigator.pushNamed(context, routeName);
+  }
+
+  static pop(BuildContext context) {
+    return Navigator.pop(context);
+  }
+}
