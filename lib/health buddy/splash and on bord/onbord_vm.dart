0df89bdf25ffd4ea3190f5a9ce93d0e3fc231a58
Bilal Hussain <bilaltoori90@gmail.com>
2022-08-04 14:07:02 +0500

    .

--- lib/health buddy/splash and on bord/onbord_vm.dart
+++ lib/health buddy/splash and on bord/onbord_vm.dart
@@ -28,24 +28,24 @@ class OnBordVM extends BaseViewModel {
 
   List<OnboardModel> screens = [
     OnboardModel(
-      image: 'assets/images/onboarding_images/img1.svg',
+      image: 'assets/images/onboarding_images/image1.png',
       titleText: "24/7 Round-the-Clock- Service",
       subtitleText:
           "24/7 Medical services available for all chronic, acute & palliative care ",
     ),
     OnboardModel(
-      image: 'assets/images/onboarding_images/img2.svg',
+      image: 'assets/images/onboarding_images/image2.png',
       titleText: "Advance Urgent Care",
       subtitleText: "A complete solution for all immediate care needs.",
     ),
     OnboardModel(
-      image: 'assets/images/onboarding_images/img3.svg',
+      image: 'assets/images/onboarding_images/Telemedicine.png',
       titleText: "Online Teleconsultation",
       subtitleText:
           "Book video call appointment & connect with experienced doctors over the phone.",
     ),
     OnboardModel(
-      image: 'assets/images/onboarding_images/img4.svg',
+      image: 'assets/images/onboarding_images/image4.png',
       titleText: "Lab With Instant Results",
       subtitleText:
           "Quick & easy laboratory tests, diagnostics with instant results",
