044a62d117e928c2779cd88f6e4169fb73cc4fbb
Salman Khan cute <sulimanaqeel714@gmail.com>
2022-08-05 15:10:18 +0500

    .

--- lib/health_buddy/home/appointments/payment/payment_method.dart
+++ lib/health_buddy/home/appointments/payment/payment_method.dart
@@ -29,43 +29,48 @@ class PaymentMethodCell extends StatelessWidget {
   Widget paymentMehod(String imageUrl, String subtitle, int value) {
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, bottom: 8),
-      child: Container(
-        decoration: BoxDecoration(
-            color: whiteColor,
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(width: 1, color: grey100),
-            boxShadow: cardShadow),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SvgPicture.asset(imageUrl),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    subtitle,
-                    style: titleTextTwoStyle,
-                  ),
-                  const Spacer(),
-                  Container(
-                    width: 20,
-                    height: 20,
-                    child: Radio(
-                        value: value,
-                        fillColor:
-                            MaterialStateColor.resolveWith((states) => blue500),
-                        focusColor:
-                            MaterialStateColor.resolveWith((states) => grey300),
-                        groupValue: viewModel.radioValue,
-                        onChanged: viewModel.handleValue),
-                  ),
-                ],
-              )
-            ],
+      child: GestureDetector(
+        onTap: () {
+          viewModel.handleValue(value);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(width: 1, color: grey100),
+              boxShadow: cardShadow),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SvgPicture.asset(imageUrl),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      subtitle,
+                      style: titleTextTwoStyle,
+                    ),
+                    const Spacer(),
+                    Container(
+                      width: 20,
+                      height: 20,
+                      child: Radio(
+                          value: value,
+                          fillColor: MaterialStateColor.resolveWith(
+                              (states) => blue500),
+                          focusColor: MaterialStateColor.resolveWith(
+                              (states) => grey300),
+                          groupValue: viewModel.radioValue,
+                          onChanged: viewModel.handleValue),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
